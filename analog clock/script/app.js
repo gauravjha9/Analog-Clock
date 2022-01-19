e63327e4941460bbecb0a24873560f69d7eec499
Gaurav Jha <51743345+gauravjha9@users.noreply.github.com>
2022-01-19 19:11:08 +0530

    Add files via upload

---
+++ analog clock/script/app.js
@@ -0,0 +1,22 @@
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+const setClock = () => {
+  const currentDate = new Date();
+  const seconds = currentDate.getSeconds() / 60;
+  const minutes = (seconds + currentDate.getMinutes()) / 60;
+  const hours = (minutes + currentDate.getHours()) / 12;
+
+  setRotation(secondHand, seconds);
+  setRotation(minuteHand, minutes);
+  setRotation(hourHand, hours);
+};
+
+setInterval(setClock, 1000);
+
+const setRotation = (element, rotationRatio) => {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+};
+
+console.log("Hello world");
